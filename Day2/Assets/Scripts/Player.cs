f6b13c56bfb27de192ca7ec3cdb952f28940518d
Jaden Balogh <jadenbalogh@gmail.com>
2020-05-02 13:38:10 -0700

    Add day 2

---
+++ Day2/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    ///<summary>Move the player based on the user keyboard input.</summary>
+    private void Move()
+    {
+        
+    }
+}
