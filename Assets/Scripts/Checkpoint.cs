f5a1e9685da68100449d81c441e993fa8dfd5fc4
Jaden Balogh <jadenbalogh@gmail.com>
2020-05-02 20:26:54 -0700

    Add checkpoint and basic UI

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -17,5 +17,6 @@ public class Checkpoint : MonoBehaviour
     void OnTriggerEnter2D()
     {
         Debug.Log("Checkpoint reached!");
+        LevelController.instance.EndGame();
     }
 }
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static LevelController instance;
+
+    public string endMessage;
+
+    public TimeDisplay timeDisplay;
+    public MessageDisplay messageDisplay;
+
+    private float gameTimer;
+    private bool isTimerActive;
+
+    void Awake()
     {
-        
+        // Enforce a singleton pattern for the LevelController object
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+        instance = this;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        isTimerActive = true;
+    }
+
     void Update()
     {
-        
+        if (isTimerActive)
+        {
+            gameTimer += Time.deltaTime;
+            timeDisplay.SetTime(gameTimer);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    public void EndGame()
+    {
+        isTimerActive = false;
+        timeDisplay.SetCompleted();
+        messageDisplay.SetMessage(endMessage);
     }
 }
